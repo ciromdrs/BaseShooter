2d593c0c04c2a7d1fa68fdefa32d1e5a8128eaee
Ciro <ciromoraismedeiros@gmail.com>
2022-06-14 22:55:49 -0300

    Clean Player script

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,14 +1,4 @@
 extends "res://scripts/Character.gd"
 
 
-func _process(_delta):
-	var animplayer = $Model/AnimationPlayer
-	if state == ATTACKING:
-		animplayer.current_animation = "attacking"
-		if almost_finished_animation():
-			state = IDLE
-		return
-	if state == IDLE:
-		animplayer.current_animation = "idle"
-	elif state == WALKING:
-		animplayer.current_animation = "walking"
+
