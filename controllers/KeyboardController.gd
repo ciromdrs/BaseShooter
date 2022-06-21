b24d909c55d362b4539898d8bc5459e0132a7aa2
Ciro <ciromoraismedeiros@gmail.com>
2022-06-21 14:02:30 -0300

    Add scene reset action for debugging

--- controllers/KeyboardController.gd
+++ controllers/KeyboardController.gd
@@ -57,6 +57,9 @@ func _process(_delta):
 	
 	if Input.is_action_just_pressed("toggle_fullscreen"):
 		OS.window_fullscreen = not OS.window_fullscreen
+	
+	if OS.is_debug_build() and Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
 
 
 
