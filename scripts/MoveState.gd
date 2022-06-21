e124158b2a3ba7c59fdd85d870fd9f9bd7f9d360
Ciro <ciromoraismedeiros@gmail.com>
2022-06-21 14:19:23 -0300

    Add blank line to MoveState

--- scripts/MoveState.gd
+++ scripts/MoveState.gd
@@ -6,6 +6,7 @@ var move_speed: float
 var movement: Vector3
 var target: Spatial
 
+
 func process(delta: float) -> void:
 	state_machine.owner.target = target
 	state_machine.owner.move_and_collide(movement * move_speed * delta)
