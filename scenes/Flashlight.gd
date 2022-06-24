965a9771bbf1b105364e3a8d59661ff939327de4
Ciro <ciromoraismedeiros@gmail.com>
2022-06-24 09:55:16 -0300

    Create Flashlight

--- scenes/Flashlight.gd
+++ scenes/Flashlight.gd
@@ -4,4 +4,4 @@ extends Spatial
 func _process(_delta: float) -> void:
 	var target = get_parent().get("target")
 	if target:
-		$SpotLight.look_at(target.global_transform.origin, Vector3.UP)
+		look_at(target.global_transform.origin, Vector3.UP)
