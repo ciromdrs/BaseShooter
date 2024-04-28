6cb99cf1058ea6ed1af68f1bf47c5248558ae4da
Ciro <ciromoraismedeiros@gmail.com>
2024-04-28 07:47:03 -0300

    Small changes in MoveMinigame

--- assets/DemoBlock.gd
+++ assets/DemoBlock.gd
@@ -2,9 +2,15 @@
 
 extends MeshInstance3D
 
-@export var color:= Color(1,1,1):
+@export var color := Color(1,1,1):
 	set(new):
 		color = new
 		var material := StandardMaterial3D.new()
 		material.albedo_color = new
 		set_surface_override_material(0, material)
+
+@export var size := Vector3(1,1,1):
+	set(new):
+		size = new
+		var shape = $StaticBody3D/CollisionShape3D
+		shape.size = new
