248f8720cf4045e028ff69226e12b9cbf7363868
Ciro <ciromoraismedeiros@gmail.com>
2024-05-28 14:08:02 -0300

    Add aim

--- weapons/Weapon.gd
+++ weapons/Weapon.gd
@@ -2,17 +2,24 @@
 class_name Weapon
 extends Node3D
 
+
+const MAX_APERTURE = PI
+
 ## In meters.
 @export var range_: float = 13
 
 ## Damage.
 @export var damage: int = 30
 
+## Accuracy.
+@export_range(.01, 1) var accuracy: float = .7
+
 ## Indicates if just shot.
 var just_shot: bool = false
 
 
 func _ready():
+	_update_aim()
 	for r in $RayCasts.get_children():
 		r.scale = Vector3(1, self.range_, 1)
 
@@ -34,3 +41,8 @@ func _shoot_process():
 				var collider = r.get_collider()
 				if collider is Character:
 					collider.take_damage(damage)
+
+func _update_aim():
+	var angle = (1 - accuracy)**2 * MAX_APERTURE / 2
+	$LeftBound.rotate(Vector3.UP, -angle)
+	$RightBound.rotate(Vector3.UP, angle)
